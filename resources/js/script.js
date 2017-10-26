7512c89ef094854a388fb86a06b21c9eee989a5a
n3m3sis42 <n3m3sis42@gmail.com>
2017-10-26 16:15:58 -0400

    added smooth scroll for header buttons and navbar

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
     
+    /* Switch between main navbar and sticky navigation when page is scrolled */    
     $('.js--section-features').waypoint(function(direction) {
         if (direction == 'down') {
             $('nav').addClass('sticky');
@@ -8,12 +9,34 @@ $(document).ready(function() {
         }
     }, {
         offset: '60px;'
-    })
+    });
     
-//    var waypoints = $('#handler-first').waypoint(function(direction) {
-//      notify(this.element.id + ' hit 25% from top of window') 
-//    }, {
-//      offset: '25%'
-//    })
+    /* Scroll to correct section when buttons in header are clicked */
+    $('.js--scroll-to-plans').click(function() {
+        $('html, body').animate({ scrollTop: $('.js--section-plans').offset().top }, 1000);
+    });
+    
+    $('.js--scroll-to-start').click(function() {
+        $('html, body').animate({ scrollTop: $('.js--section-features').offset().top }, 1000);
+    });
 
+    /* Scroll to correct section when links in navbar are clicked */
+    $(function() {
+        $('a[href*="#"]')
+          .not('[href="#"]')
+          .click(function(event) {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+              var target = $(this.hash);
+              target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+              if (target.length) {
+                $('html, body').animate({
+                  scrollTop: target.offset().top
+                }, 1000);
+                return false;
+              }
+            }
+          });    
+    });
+    
+    
 });
