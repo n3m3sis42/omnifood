9d78ecebcdd3bf7f0ad365f4ed3dad3789e6c041
n3m3sis42 <n3m3sis42@gmail.com>
2017-10-28 14:48:59 -0400

    added responsive navigation

--- resources/js/script.js
+++ resources/js/script.js
@@ -43,25 +43,67 @@ $(document).ready(function() {
         $('.js--waypoint-1').addClass('animated fadeIn');
     }, {
         offset: '50%'
-    })
+    });
     /* Animate app-screen image on scroll */
     $('.js--waypoint-2').waypoint(function(direction) {
         $('.js--waypoint-2').addClass('animated fadeInUp');
     }, {
         offset: '50%'
-    })
+    });
     /* Animate cities on scroll */    
     $('.js--waypoint-3').waypoint(function(direction) {
         $('.js--waypoint-3').addClass('animated fadeIn');
     }, {
         offset: '50%'
-    })
+    });
     /* Animate pricing plans on scroll */    
     $('.js--waypoint-4').waypoint(function(direction) {
         $('.js--waypoint-4').addClass('animated pulse');
     }, {
         offset: '50%'
-    })
+    });
 
+    /* Mobile nav */
+    $('.js--nav-icon, .js--main-nav a, .logo-black').click(function(element) {
+        var nav = $('.js--main-nav');
+        var icon = $('.js--nav-icon i');
+        var clicked = element.target.classname;
+        
+        // exit if user clicked the logo and not the menu button
+        if (icon.hasClass('ion-navicon-round') && clicked == 'logo-black') {
+            return;
+        }
+        
+        // open or close mobile nav menu screen width is less than 768px
+        if ($(window).width() < 768){
+            nav.slideToggle(200);
+        }
+        
+        // toggle icon for menu button when it is clicked
+        if (icon.hasClass('ion-navicon-round')) {
+            icon.addClass('ion-close-round');
+            icon.removeClass('ion-navicon-round');
+        } else {
+            icon.addClass('ion-navicon-round');
+            icon.removeClass('ion-close-round');            
+        }        
+        
+    });
+    
+    $(window).resize(function(){
+        var nav = $('.js--main-nav');
+        var icon = $('.js--nav-icon i');
+
+        if ($(window).width() > 767){
+            nav.css("display", "block");
+            icon.addClass('ion-close-round');
+            icon.removeClass('ion-navicon-round');
+        } else {
+            nav.css("display", "none");
+            icon.addClass('ion-navicon-round');
+            icon.removeClass('ion-close-round');
+        }
+
+    });    
     
 });
